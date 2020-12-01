5ea6cbd8d78596a61d42bf37b6851ca364d70e9c
darpan2899 <49209125+darpan2899@users.noreply.github.com>
2020-12-01 23:05:48 +0530

    Add files via upload

---
+++ Hibernate_example/src/com/drv/DemoHib/Student.java
@@ -0,0 +1,50 @@
+package com.drv.DemoHib;
+
+
+	public class Student {  
+		 int Sem;
+		 double id;
+		float CPI;  
+		 String Name,username,password;  
+		  
+		public double getId() {  
+		    return id;  
+		}  
+		public void setId(double id) {  
+		    this.id = id;  
+		} 
+		public int getSem() {  
+		    return Sem;  
+		}  
+		public void setSem (int Sem) {  
+		    this.Sem = Sem;  
+		}   
+		public float getCPI() {  
+		    return CPI;  
+		}  
+		public void setCPI (float CPI) {  
+		    this.CPI = CPI;  
+		}   
+		public String getName() {  
+		    return Name;  
+		}  
+		public void setName(String Name) {  
+		    this.Name = Name;  
+		}  
+		public String getusername() {  
+		    return username;  
+		}  
+		public void setusername(String username) {  
+		    this.username = username;  
+		}  
+
+		public void setpassword(String password) {  
+		    this.password = password;  
+		}  
+		public String getpassword() {  
+		    return password ;  
+		}  
+		}  
+
+
+
